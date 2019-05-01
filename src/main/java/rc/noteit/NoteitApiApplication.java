bc927b290f037a8f7994f3bcb98b0bdd63f50c4c
Ali <ali.benlemlih@hotmail.com>
2019-05-01 15:13:10 +0100

    Initial commit

---
+++ src/main/java/rc/noteit/NoteitApiApplication.java
@@ -0,0 +1,13 @@
+package rc.noteit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteitApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteitApiApplication.class, args);
+	}
+
+}
